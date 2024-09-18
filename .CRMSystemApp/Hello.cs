4219a5ae23e50c956abecb6af59ffcf02b3b3c04
marcusjacobsen <S215799@dtu.dk>
2024-09-18 21:44:34 +0200

    add some more stuff

---
+++ .CRMSystemApp/Hello.cs
@@ -0,0 +1,14 @@
+public class Hello
+{
+    /// <summary>
+    /// Returns a greeting message.
+    /// </summary>
+    /// <returns>
+    /// A string containing a greeting message.
+    /// </returns>
+    public string SayHello()
+    {
+        
+        return "Hello, World!";
+    }
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,27 +1,77 @@
 using BlazorApp.Components;
+using BlazorApp.Persistence;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        builder.Services.AddHttpContextAccessor();
+
+
+        builder.Services.AddDbContext<UserDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+
+        builder.Services.AddAuthentication("CookieAuth")
+            .AddCookie("CookieAuth", config =>
+            {
+                config.Cookie.Name = "UserLoginCookie";
+                config.LoginPath = "/login";
+            });
+
+        var app = builder.Build();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+            SeedDatabase(dbContext);
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+
+    static void SeedDatabase(UserDbContext dbContext)
+        {
+            // Ensure the database is created
+            dbContext.Database.EnsureCreated();
+
+            // Check if there are any users, if not, create a default one
+            if (!dbContext.Users.Any())
+            {
+                var defaultUser = new User
+                {
+                    Username = "admin",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("admin123")
+                };
+
+                dbContext.Users.Add(defaultUser);
+                dbContext.SaveChanges();
+            }
+        }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
