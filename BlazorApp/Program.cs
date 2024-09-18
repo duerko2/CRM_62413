838e44d115ae43c439e9a65007cc80768c9674f2
marcusjacobsen <S215799@dtu.dk>
2024-09-18 21:54:40 +0200

    add some more stuff

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,7 +1,4 @@
 using BlazorApp.Components;
-using BlazorApp.Persistence;
-using Microsoft.EntityFrameworkCore;
-
 
 internal class Program
 {
@@ -12,22 +9,9 @@ internal class Program
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
-        builder.Services.AddHttpContextAccessor();
 
-
-        builder.Services.AddDbContext<UserDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
-
-        builder.Services.AddAuthentication("CookieAuth")
-            .AddCookie("CookieAuth", config =>
-            {
-                config.Cookie.Name = "UserLoginCookie";
-                config.LoginPath = "/login";
-            });
 
         var app = builder.Build();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
@@ -35,14 +19,6 @@ internal class Program
             app.UseExceptionHandler("/Error", createScopeForErrors: true);
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
-        }
-
-
-
-        using (var scope = app.Services.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-            SeedDatabase(dbContext);
         }
 
         app.UseHttpsRedirection();
@@ -55,23 +31,4 @@ internal class Program
 
         app.Run();
     }
-
-    static void SeedDatabase(UserDbContext dbContext)
-        {
-            // Ensure the database is created
-            dbContext.Database.EnsureCreated();
-
-            // Check if there are any users, if not, create a default one
-            if (!dbContext.Users.Any())
-            {
-                var defaultUser = new User
-                {
-                    Username = "admin",
-                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("admin123")
-                };
-
-                dbContext.Users.Add(defaultUser);
-                dbContext.SaveChanges();
-            }
-        }
 }
