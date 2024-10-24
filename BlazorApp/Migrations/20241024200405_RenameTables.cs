b3eaa30f7b7208428d0c0feb5faa82372752af9c
marcusjacobsen <S215799@dtu.dk>
2024-10-24 23:41:57 +0200

    migrations

---
+++ BlazorApp/Migrations/20241024200405_RenameTables.cs
@@ -0,0 +1,308 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_User_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pipeline_Campaign_CampaignId",
+                table: "Pipeline");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pipeline_Contacts_ContactId",
+                table: "Pipeline");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_Pipeline_PipelineId",
+                table: "Task");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Company_CompanyId",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Task",
+                table: "Task");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pipeline",
+                table: "Pipeline");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Company",
+                table: "Company");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Campaign",
+                table: "Campaign");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Task",
+                newName: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Pipeline",
+                newName: "Pipelines");
+
+            migrationBuilder.RenameTable(
+                name: "Company",
+                newName: "Companies");
+
+            migrationBuilder.RenameTable(
+                name: "Campaign",
+                newName: "Campaigns");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_CompanyId",
+                table: "Users",
+                newName: "IX_Users_CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Task_PipelineId",
+                table: "Tasks",
+                newName: "IX_Tasks_PipelineId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pipeline_ContactId",
+                table: "Pipelines",
+                newName: "IX_Pipelines_ContactId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pipeline_CampaignId",
+                table: "Pipelines",
+                newName: "IX_Pipelines_CampaignId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pipelines",
+                table: "Pipelines",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Campaigns",
+                table: "Campaigns",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pipelines_Campaigns_CampaignId",
+                table: "Pipelines",
+                column: "CampaignId",
+                principalTable: "Campaigns",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pipelines_Contacts_ContactId",
+                table: "Pipelines",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Pipelines_PipelineId",
+                table: "Tasks",
+                column: "PipelineId",
+                principalTable: "Pipelines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Companies_CompanyId",
+                table: "Users",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pipelines_Campaigns_CampaignId",
+                table: "Pipelines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pipelines_Contacts_ContactId",
+                table: "Pipelines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Pipelines_PipelineId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Companies_CompanyId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pipelines",
+                table: "Pipelines");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Campaigns",
+                table: "Campaigns");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "Task");
+
+            migrationBuilder.RenameTable(
+                name: "Pipelines",
+                newName: "Pipeline");
+
+            migrationBuilder.RenameTable(
+                name: "Companies",
+                newName: "Company");
+
+            migrationBuilder.RenameTable(
+                name: "Campaigns",
+                newName: "Campaign");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_CompanyId",
+                table: "User",
+                newName: "IX_User_CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_PipelineId",
+                table: "Task",
+                newName: "IX_Task_PipelineId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pipelines_ContactId",
+                table: "Pipeline",
+                newName: "IX_Pipeline_ContactId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pipelines_CampaignId",
+                table: "Pipeline",
+                newName: "IX_Pipeline_CampaignId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Task",
+                table: "Task",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pipeline",
+                table: "Pipeline",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Company",
+                table: "Company",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Campaign",
+                table: "Campaign",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_User_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pipeline_Campaign_CampaignId",
+                table: "Pipeline",
+                column: "CampaignId",
+                principalTable: "Campaign",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pipeline_Contacts_ContactId",
+                table: "Pipeline",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_Pipeline_PipelineId",
+                table: "Task",
+                column: "PipelineId",
+                principalTable: "Pipeline",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Company_CompanyId",
+                table: "User",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
