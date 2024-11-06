21c19f4188c6ef77d292c2d7590d320fd172dbd3
Joakim Anker Kruse <joakimkruse@hotmail.com>
2024-11-06 14:40:04 +0100

    Changed string to DateTime

--- BlazorApp/Persistence/Entities/Task.cs
+++ BlazorApp/Persistence/Entities/Task.cs
@@ -4,8 +4,8 @@ public class Task
 {
     public int Id { get; set; }
     public string Description { get; set; }
-    public string CreatedDate { get; set; }
-    public string Deadline { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime Deadline { get; set; }
     public int PipelineId { get; set; }
     public virtual Pipeline Pipeline { get; set; }
 }
